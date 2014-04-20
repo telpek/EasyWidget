319fa2774ea45b310a1167d541ed9ed58553f721
Askar Telpekbayev <telpekbayev@yandex.ru>
2014-04-20 12:20:49 +0600

    Several improvements
    I’ve just found out fresh info at stackoverflow and added new lines of
    code

--- gen/kz/telp/easywidget/R.java
+++ gen/kz/telp/easywidget/R.java
@@ -994,10 +994,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f05003e;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f05003c;
+        public static final int button2=0x7f05003d;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1143,7 +1145,6 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000f;
     }
     public static final class style {
         /** 
--- src/kz/telp/easywidget/MainActivity.java
+++ src/kz/telp/easywidget/MainActivity.java
@@ -1,8 +1,5 @@
 package kz.telp.easywidget;
 
-import kz.telp.settings.wifi.WifiApEnabler;
-import kz.telp.settings.wifi.WifiApDialog;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -13,20 +10,24 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.AssetManager;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.SystemProperties;
+
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.webkit.WebView;
+import android.widget.ToggleButton;
 
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -34,13 +35,18 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.Locale;
 import android.view.Menu;
 
+import kz.telp.easywidget.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	/** Called when the activity is first created. */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		
 	}
 
 	@Override
@@ -49,5 +55,41 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		if (v.getId() == tbWiFi.getId()) {
+			wifi.setWifiEnabled(tbWiFi.isChecked());
+		}
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		initButton();
+	}
+	
+	private void initButton() {
+		if
+		(isConnected(this, ConnectivityManager.TYPE_WIFI)) {
+			tbWiFi.setChecked(true);
+		}
+	}
+	
+	private static ConnectivityManager connectivityManager;
+	private ToggleButton tbWiFi;
+	private WifiManager wifi;
+	
+	private static boolean 
+	isConnected(Context context, int networkType) {
+		NetworkInfo networkInfo = null;
+		if (connectivityManager != null) {
+			networkInfo = connectivityManager.getNetworkInfo(networkType);
+		}
+		return networkInfo == null ? false
+				: networkInfo.isConnected();
+	}
+	
 
 }
